d800aabac2c777e3e6d788c37b368c0f670946b2
hugo098 <hi.rodriguez1998@gmail.com>
2023-05-19 17:23:54 -0400

    express-tutorial

---
+++ 2-express-tutorial/final/3-express-basics.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Home page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("<h1>About page</h1>");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000...");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
